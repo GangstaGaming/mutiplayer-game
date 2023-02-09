bac8f1a2be28b6f163b08df54e166dd0e3e76c70
Gavin <117520546+GangstaGaming@users.noreply.github.com>
2023-02-09 13:23:50 -0500

    Restore '2/9/2023, 1:22:30 PM Initialize empty repo'

--- main.ts
+++
@@ -1,8 +0,0 @@
-namespace SpriteKind {
-    export const Ball = SpriteKind.create()
-    export const wall = SpriteKind.create()
-    export const Text = SpriteKind.create()
-}
-sprites.onOverlap(SpriteKind.Ball, SpriteKind.wall, function (sprite, otherSprite) {
-	
-})
